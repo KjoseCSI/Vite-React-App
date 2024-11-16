c1a52ce071f4a8969905fcbd848c305ccaef97c6
KjoseCSI <joserubenchicaiza@gmail.com>
2024-11-16 16:39:33 -0500

    Update button1

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-
 function App() {
   const [count, setCount] = useState(0)
 
@@ -22,7 +21,6 @@ function App() {
         <button className='button-plus-1' onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>jajajaja</p>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
