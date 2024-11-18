113380066389cc0dec19892d1904617b4a95c67b
KjoseCSI <joserubenchicaiza@gmail.com>
2024-11-17 22:31:13 -0500

    API Consume success
    finaly fix almost problems here

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Button } from './components'
 import { useFetch } from './hooks'
 
 const url = 'https://api.nasa.gov/planetary/apod?api_key=lXCC9iJUM8GQmio13ZxGq450nR3NitXeH1YeVPPx'
+const url2 = 'https://v2.jokeapi.dev/joke/Any?lang=es'
 
 interface Data {
   date: string;
@@ -14,16 +15,17 @@ interface Data {
   title: string;
   url: string;
 }
-type DataArray = Data[];
 
 function App() {
   const [count, setCount] = useState(0)
-  const {data, error, loading} = useFetch<DataArray>(url)
+  const {data, error, loading} = useFetch<Data>(url)
 
   const countMore = () => {
     setCount((count) => count + 1)
   }
 
+  if(loading) return <p>Loading...</p>
+  if(error) return <p>Error fetching data: {error.message}</p>
   
   return (
     <>
@@ -44,15 +46,13 @@ function App() {
       <div className='divided-container'>
         <div>
           First API: NASA APIs
-          <br>APOD: Astronomy Picture of the Day.</br>
+          <br/>APOD: Astronomy Picture of the Day.<br/>
         </div>
         <div>
-          <ul>
-            {data?.map((data) => (<li> {data.explanation} 
-              <br> {data.hdurl}</br>
-              </li>))}
-          </ul>
-        </div>
+              <h2>{data?.title}</h2>
+              <p>{data?.explanation}</p>
+              {data?.hdurl && <img src={data.hdurl} alt={data.title} style={{ maxWidth: '100%' }} />}
+            </div>
       </div>
     </>
   )
