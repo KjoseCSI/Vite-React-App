8127a963096bd831620fff18e6cfff8facfacf3d
KjoseCSI <joserubenchicaiza@gmail.com>
2024-11-18 21:38:22 -0500

    Update with skeletonLoader Test 0.1
    i dont know how work

--- src/components/InterfaceAPI/NasaAPI.tsx
+++ src/components/InterfaceAPI/NasaAPI.tsx
@@ -1,7 +1,9 @@
 import "./Table1x2.css"
 import { useFetch } from '../../hooks'
+import Skeleton, { SkeletonTheme } from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
 
-const url = 'https://api.nasa.gov/planetary/apod?api_key=lXCC9iJUM8GQmio13ZxGq450nR3NitXeH1YeVPPx'
+const url = 'https://api.nasa.gov/planetary/apod?api_key=lXCC9iJUM8GQmio13ZxGq450nR3NitXeH1YeVPPx-ajaa'
 
 interface Props{
     date: string;
@@ -22,17 +24,40 @@ export const NasaAPI = () => {
      )   
     }
 
-    if(error) return <p>Error fetching data with NasaAPI: {error?.message }</p>
+    //if(error) return <p>Error fetching data with NasaAPI: {error?.message }</p>
+
+    if(error) return (
+    <div className='divided-container'>
+     <div>
+      First API: NASA APIs
+      <br/>APOD: Astronomy Picture of the Day.<br/>
+     </div >
+     <div
+      style={{
+        border: '1px solid #ccc',
+        display: 'block',
+        lineHeight: 2,
+        padding: '1rem',
+        marginBottom: '0.5rem',
+        width: 100,
+      }}
+    >
+     <p>
+      <Skeleton count={10} />
+    </p>
+    </div>
+    </div>)
 
     return(
-        <div className='divided-container'>
+      <div className='divided-container'>
         <div>
           First API: NASA APIs
           <br/>APOD: Astronomy Picture of the Day.<br/>
         </div>
+        
         <div>
-              <h2>{data?.title}</h2>
-              <p>{data?.explanation}</p>
+              <h2>{data?.title || <Skeleton />}</h2>
+              <p>{data?.explanation || <Skeleton count={10}/>}</p>
               {data?.hdurl && <img src={data.hdurl} alt={data.title} style={{ maxWidth: '100%' }} />}
             </div>
       </div>
